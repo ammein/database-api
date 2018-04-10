3191d37dd482772f0b0514b0a14ab1b26bc51033
Amin Shazrin <aminshazrin@yahoo.com>
2018-04-10 17:59:11 +0800

    Done add data to database

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,43 @@
+const MongoClient = require('mongodb').MongoClient;
+// Pull off the mongoCLient.
+
+
+// To connect to database 
+// Takes two argument , First take a url , second callback function (If connected succeeded , console log)
+// To connect to database , we have to use MongoDB protocol = mongodb://
+// The port is your mongodb port
+// Then we have to specify WHICH database we want to connect to by using forward slash '/'
+MongoClient.connect('mongodb://localhost:27017/TodoApp' , (err , client) => {
+    if (err){
+        return console.log("Unable to connect to server");
+    }
+    console.log("Connected to MongoDB server");
+    // .collection(string)
+    // .insertOne(object , (callback)) -- Insert New Document
+    // Callback -> Fired or Failed
+    // client.db(callingReferenceFor)
+    const db = client.db('TodoApp');
+    // db.collection('Todos').insertOne({
+    //     text : 'Something to do',
+    //     completed : false
+    // },(err , result) => {
+    //     if(err){
+    //         return console.log("Unable to insert todo" , err);
+    //     }
+    //     console.log("Success" , JSON.stringify(result.ops , undefined , 2));
+    //     // .ops -> store all of the docs that were inserted
+    // });
+
+    db.collection('Users').insertOne({
+        name : 'Amin Shazrin',
+        age : 24,
+        location : 'No 28 Jalan SG 7/18 Taman Sri Gombak'
+    }, (err , result) =>{
+        if(err)
+        {
+            return console.log("Unable to insert user" , err);
+        }
+        console.log(JSON.stringify(result.ops , undefined , 2));
+    });
+    client.close(); // Close Connection DB Server
+});
