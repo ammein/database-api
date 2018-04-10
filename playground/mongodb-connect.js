0ff356c79cb39f53de6239c45b7c175eff910a06
Amin Shazrin <aminshazrin@yahoo.com>
2018-04-10 18:32:56 +0800

    Done Object ID

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,6 +1,13 @@
-const MongoClient = require('mongodb').MongoClient;
-// Pull off the mongoCLient.
-
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient , ObjectID} = require('mongodb');
+// ES6 Restructuring Object
+var user = {name : "Amin" , age : 24};
+var {name} = user; 
+// {setWhatEver Key you want to restructre}
+// equal to = WhatEver Object You want to restructure
+// console.log(name);
+var obj = new ObjectID(); // Must create an Instance
+console.log(ObjectID);
 
 // To connect to database 
 // Takes two argument , First take a url , second callback function (If connected succeeded , console log)
@@ -28,16 +35,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp' , (err , client) => {
     //     // .ops -> store all of the docs that were inserted
     // });
 
-    db.collection('Users').insertOne({
-        name : 'Amin Shazrin',
-        age : 24,
-        location : 'No 28 Jalan SG 7/18 Taman Sri Gombak'
-    }, (err , result) =>{
-        if(err)
-        {
-            return console.log("Unable to insert user" , err);
-        }
-        console.log(JSON.stringify(result.ops , undefined , 2));
-    });
+    // db.collection('Users').insertOne({
+    //     name : 'Amin Shazrin',
+    //     age : 24,
+    //     location : 'No 28 Jalan SG 7/18 Taman Sri Gombak'
+    // }, (err , result) =>{
+    //     if(err)
+    //     {
+    //         return console.log("Unable to insert user" , err);
+    //     }
+    //     // console.log(JSON.stringify(result.ops , undefined , 2));
+    //     console.log(result.ops[0]._id.getTimestamp());
+    // });
     client.close(); // Close Connection DB Server
 });
