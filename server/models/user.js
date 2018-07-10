0bce0ae37b3d2e35be33e7f28194f2d604cf6dce
Amin Shazrin <aminshazrin@yahoo.com>
2018-07-10 23:10:50 +0800

    Done this

--- server/models/user.js
+++ server/models/user.js
@@ -76,17 +76,17 @@ UserSchema.statics.findByToken = function (token) {
     */
     try{
         decoded = jwt.verify(token , 'abc123');
+        // Using return because we are going to use promise
+        return User.findOne({
+            _id: decoded._id,
+            'tokens.token': token,
+            'tokens.access': 'auth'
+        });
     }catch (e){
         return new Promise((resolve , reject)=>{
             reject(e);
         });
     };
-    // Using return because we are going to use promise
-    return User.findOne({
-        _id : decoded._id,
-        'tokens.token' : token,
-        'tokens.access' : 'auth'
-    })
 };
 
 // before 'save'
